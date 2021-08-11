b40b55d59017d7c298ce6ada46b9517917c2b9d3
Johnatan Souza <jgs8115@gmail.com>
2021-08-11 01:03:37 -0300

    arquivo principal

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from cliente import Cliente
 
-c1 = Cliente(
-    "Fernanda", "Apolly", 45, "(11)9448-10177",
-    "apolly@gmail.com", 9417, "1340-1"
-)
 c2 = Cliente(
-    "Gabriel", "Albert", 72, "(11)9884-45677",
-    "giel.bert@gmail.com", 9417, "1341-1"
+    "Fernanda", "Apolly", 45, "(11)9448-10177", "apolly@gmail.com", 3064, "100181-1"
 )
 
-c1.conta.mostrar_detalhes()
-c1.conta.tranferir(500, c2.conta)
-c2.conta.mostrar_detalhes()
+c1 = Cliente(
+    "Gabriel", "Albert", 72, "(11)9884-45677", "giel.bert@gmail.com", 3064, "100180-1"
+
+)
+
+c1.alterar_email("fernanda.appol@gmail.com")
