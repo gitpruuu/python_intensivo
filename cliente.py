a4711bfe3a543d8441e6c1e9795bc15d62eb4cbc
Johnatan Souza <jgs8115@gmail.com>
2021-08-11 01:01:36 -0300

    alteração no import

--- cliente.py
+++ cliente.py
@@ -4,7 +4,11 @@ from conta_corrente import Conta
 
 class Cliente(Pessoa):
 
-    def __init__(self, primeiro_nome, ultimo_nome, idade, telefone, email, agencia, numero_da_conta):
+    def __init__(
+            self, primeiro_nome,
+            ultimo_nome, idade,
+            telefone, email, agencia,
+            numero_da_conta
+    ):
         super().__init__(primeiro_nome, ultimo_nome, idade, telefone, email)
-        self.conta = Conta(agencia, numero_da_conta)
-    
+        self.conta_corrente = Conta(agencia, numero_da_conta)
